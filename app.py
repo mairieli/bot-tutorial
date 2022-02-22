6fd0939dc76426a5a0d4c8adf9f142890ce3b636
Mairieli Wessel <mairieliw@gmail.com>
2022-02-23 00:36:12 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, request
+from github import Github, GithubIntegration
+
+app = Flask(__name__)
+
+app_id = ''
+
+# Read the bot certificate
+with open(
+        os.path.normpath(os.path.expanduser('bot_key.pem')),
+        'r'
+) as cert_file:
+    app_key = cert_file.read()
+    
+# Create an GitHub integration instance
+git_integration = GithubIntegration(
+    app_id,
+    app_key,
+)
+
+@app.route("/", methods=['POST'])
+def bot():
+    payload = request.json
+
+    owner = payload['repository']['owner']['login']
+    repo_name = payload['repository']['name']
+
+    git_connection = Github(
+        login_or_token=git_integration.get_access_token(
+            git_integration.get_installation(owner, repo_name).id
+        ).token
+    )
+    repo = git_connection.get_repo(f"{owner}/{repo_name}")
+
+    # Check if the event is a GitHub pull request creation event
+    if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
+        # TODO: implement the feature to greet first-time developers
+
+    return "", 204
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
