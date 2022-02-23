ef2bbd543bfb5ca6420b3eddba696f92e63484c4
Mairieli Wessel <mairieliw@gmail.com>
2022-02-23 02:23:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from github import Github, GithubIntegration
 
 app = Flask(__name__)
 
-app_id = '<your_app_number_here>'
+app_id = 174759
 
 # Read the bot certificate
 with open(
@@ -19,9 +19,24 @@ git_integration = GithubIntegration(
     app_key,
 )
 
+def pr_opened_event(repo, payload):
+    pr = repo.get_issue(number=payload['pull_request']['number'])
+    author = pr.user.login
+
+    is_first_pr = repo.get_issues(creator=author).totalCount
+
+    if is_first_pr == 1:
+        response = f"Thanks for opening this pull request, @{author}! " \
+                   f"The repository maintainers will look into it ASAP! :speech_balloon:"
+        pr.create_comment(f"{response}")
+        pr.add_to_labels("needs review")
+
 @app.route("/", methods=['POST'])
 def bot():
     payload = request.json
+
+    if not 'repository' in payload.keys():
+        return "", 204
 
     owner = payload['repository']['owner']['login']
     repo_name = payload['repository']['name']
@@ -35,7 +50,7 @@ def bot():
 
     # Check if the event is a GitHub pull request creation event
     if all(k in payload.keys() for k in ['action', 'pull_request']) and payload['action'] == 'opened':
-        # TODO: implement the feature to greet first-time developers
+        pr_opened_event(repo, payload)
 
     return "", 204
 
